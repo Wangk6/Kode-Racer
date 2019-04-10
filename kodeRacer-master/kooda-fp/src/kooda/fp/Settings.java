afcb4a76c3b83a45f61f0619845a4501c5f9218f
Kevin Wang <Wangk6@farmingdale.edu>
2019-04-09 20:54:14 -0400

    Modified main menu and added settings - need to finish fullscreen and volume

---
+++ kodeRacer-master/kooda-fp/src/kooda/fp/Settings.java
@@ -0,0 +1,30 @@
+package kooda.fp;
+
+public class Settings extends KoodaFp{
+
+	int fullScreen;
+	int volume;
+	
+	public Settings() {
+		fullScreen = 0;
+		volume = 0;
+	}
+	
+	public int getFullScreen() {
+		return fullScreen;
+	}
+
+	public void setFullScreen(int fullScreen) {
+		this.fullScreen = fullScreen;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	
+}
