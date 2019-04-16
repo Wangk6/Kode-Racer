2024810c561ef61a3138c8e4e04b6d98335537ed
Quazi Billah <info@antiantianti.com>
2019-04-15 21:06:32 -0400

    Working on level one

--- KodeRacer/src/level/LevelOne.java
+++ KodeRacer/src/level/LevelOne.java
@@ -3,9 +3,11 @@ package level;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -29,14 +31,83 @@ public class LevelOne{
 	
 	private Pane root = new Pane();
 	
-    Sprite player1 = new Sprite(300, 300, (int)width/22, (int)width/22, "random", Color.RED);
+    Sprite player1 = new Sprite(300, 300, (int)width/25, (int)width/25, "random", Color.RED); //Keep the W & H to 25, because we're moving at 5's
+    
+    //Directional Buttons
+    private Button directionUp = new Button();
+    private Button directionDown = new Button();
+    private Button directionLeft = new Button();
+    private Button directionRight = new Button();
+    
     
 	private Parent createContent() {
 		//Set the level size
 		root.setPrefSize(900, 900);
 		root.getChildren().add(player1);
+		
+		//Directional Buttons
+		
+		directionUp.setPrefWidth(50);
+		directionUp.setPrefHeight(50);
+		directionUp.setLayoutX(0);
+		directionUp.setLayoutY(0);
+		
+		directionDown.setPrefWidth(50);
+		directionDown.setPrefHeight(50);
+		directionDown.setLayoutX(50);
+		directionDown.setLayoutY(0);
+		
+		directionLeft.setPrefWidth(50);
+		directionLeft.setPrefHeight(50);
+		directionLeft.setLayoutX(100);
+		directionLeft.setLayoutY(0);
+		
+		directionRight.setPrefWidth(50);
+		directionRight.setPrefHeight(50);
+		directionRight.setLayoutX(150);
+		directionRight.setLayoutY(0);
+		
+		
+		
+		root.getChildren().add(directionUp); 
+		root.getChildren().add(directionDown);
+		root.getChildren().add(directionLeft);
+		root.getChildren().add(directionRight);
+		
+		directionUp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	player1.moveUp();
+            }
+        });
+		
+		directionDown.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	player1.moveDown();
+            }
+        });
+		
+		directionLeft.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	player1.moveLeft();
+            }
+        });
+		
+		directionRight.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	player1.moveRight();
+            }
+        });
+		
+		//Directional Buttons
+		
+		
 		return root;
 	}
+	
 	public void start() {
 		System.out.println(Settings.getFullScreen());
 		if (Settings.getFullScreen() == 1) {
